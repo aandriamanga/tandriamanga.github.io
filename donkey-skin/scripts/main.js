e6997e4096deba9ca9db12c0d588f3c60996a3ae
Unknown <a.tojo.antonio@gmail.com>
2018-05-08 21:57:10 +0200

    Parallax background done

--- donkey-skin/scripts/main.js
+++ donkey-skin/scripts/main.js
@@ -1,5 +1,3 @@
 $(document).ready(function(){		
-  $('#slide1').parallax("center", 0, 0.1, true);
-  $('#slide2').parallax("center", 1000, 0.1, true);
-  $('#slide3').parallax("center", 3000, 0.1, true);
+  
 })
